f22d59f5805315d3662abf57ed69c20b3424b5ec
varma300 <rahulvarma300@gmail.com>
2023-06-17 08:31:19 +0530

    Added Models

--- testapp/views.py
+++ testapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from datetime import datetime
+from post.models import Post
 # Create your views here.
 
 def welcome(request):
-    return render(request, "testapp/welcome.html")
+    return render(request, "testapp/welcome.html",
+                  {"current_time": datetime.now(),
+                "posts": Post.objects.all(),
+                "num_post": Post.objects.count()})
